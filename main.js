fc5549a1c388bdb66cfa981ade662201332f9b6e
pcprotechau <pcprotechau@gmail.com>
2023-09-29 18:11:22 +1000

    main edits, waiting on more builds

--- main.js
+++ main.js
@@ -9,18 +9,3 @@ window.addEventListener('scroll', function() {
 hamburgerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open');
 })
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
